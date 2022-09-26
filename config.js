773e78808d9a1897aa58b6401da58e36dda1c0eb
Reza11Ofc <113843050+Reza11Ofc@users.noreply.github.com>
2022-09-26 08:19:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285875158363']
-global.ownernomer = "6285875158363"
-global.premium = ['6285875158363']
-global.packname = 'Sticker  Naze'
-global.author = 'Naze'
+global.owner = ['']
+global.ownernomer = "6289606790112"
+global.premium = ['6289606790112']
+global.packname = 'Sticker  EzaBot-MD'
+global.author = 'RezaOfc'
 global.sessionName = 'nazedev'
 global.prefa = ['','!','.','#','&']
 global.sp = ''
